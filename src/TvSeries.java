483302fcbb5423d2a1a7c5c3f5c8051d6d5ad45b
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2024-10-24 10:51:01 +0200

    sakertyp

--- src/TvSeries.java
+++ src/TvSeries.java
@@ -3,29 +3,38 @@ import java.util.Scanner;
 
 public class TvSeries {
 
-        private String name;
-        private ArrayList<Integer> episodes = new ArrayList<>();
-        private int rating;
+    private String name;
+    private ArrayList<Integer> episodes = new ArrayList<>();
+    private int rating;
 
 
-        public TvSeries(String inName) {
+    public TvSeries() {
         Scanner myScan = new Scanner(System.in);
-            System.out.println("Please enter the name of the tv series: ");
-            name = myScan.nextLine();
-        }
-
-        public void AddEpisodes(int numOfEpisodes, int seasons) {
-
-        }
-
-        public void ChangeRating(int NewRating) {
-
-        }
-
-        public void PrintInfo() {
-
-        }
-
-
+        System.out.println("Please enter the name of the tv series: ");
+        name = myScan.nextLine();
     }
+
+    public TvSeries(String inName) {
+        name = inName;
+    }
+
+    public void AddEpisodes(int numOfEpisodes, int season) {
+        if (season > episodes.size()) {
+            for (int i = 0; i < season - 1; i++) {
+                episodes.add(0);
+            }
+        }
+        episodes.add(season - 1, numOfEpisodes);
+    }
+
+    public void ChangeRating(int newRating) {
+        rating = newRating;
+    }
+
+    public void PrintInfo() {
+        System.out.println("Name: " + name +",reating"+ rating);
+    }
+
+
+}
 
