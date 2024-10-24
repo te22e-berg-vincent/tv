4875430c3e482382930f8cb05926110d33dd8fed
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2024-10-24 13:46:39 +0200

    sakertypyit  jhkvhly

--- src/TvMain.java
+++ src/TvMain.java
@@ -1,6 +1,16 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TvMain {
 
+    private ArrayList<TvSeries> shows = new ArrayList<>();
+    Scanner myScan = new Scanner(System.in);
 
+    public TvMain() {
+
+    }
+
+    private void startProgram() {
+
+    }
 }
